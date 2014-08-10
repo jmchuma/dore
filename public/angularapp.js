148179f86693b14827a240c3424e913b8867c157
Jose M. Chumo Mata <jmchuma@gmail.com>
2014-08-10 22:33:28 +0200

    Use the API as data source
    * Delete json file
    * Update data source URI

--- public/angularapp.js
+++ public/angularapp.js
@@ -1,12 +1,12 @@
 var app = angular.module('doreApp', []);
 
 app.controller('mainController', function($scope, $http) {
-	$http.get("./data.json")
+	$http.get("/api/movies")
 		.success(function(response) {
 			console.log(response);
-			$scope.month = response.month;
-			$scope.year = response.year;
-			$scope.movies = response.movies;
+			//$scope.month = response.month;
+			//$scope.year = response.year;
+			$scope.movies = response;
 		});
 });
 
