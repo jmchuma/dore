5cbfeb2b043432ba8bbc6ad6e74d2d6ca7bf4545
Jose M. Chumo Mata <jmchuma@gmail.com>
2014-08-26 15:14:37 +0200

    Fuse movie form controllers
    * Fuse editMovie controller into addMovieController.
    * Add a service to process the movie creation form.
    * Use the date controllers on the interface to show and get data.
    
    ERROR
    
    * Explicit declaration of the dependencies for ProcessMovieFormService results
      in the framework not detecting it.
    * Implicit declaration of the dependencies for ProcessMovieFormService results
      in empty $scope in the service.

--- public/angularapp.js
+++ public/angularapp.js
@@ -11,7 +11,39 @@ angular.module('doreApp')
     this.remove = function(set, index) {
         set.splice(index, 1);
     };
-});
+})
+
+.service('ProcessMovieFormService',
+        ['$scope', '$http', '$routeParams', '$location',
+        function($scope, $http, $routeParams, $location) {
+    this.processor = function() {
+        for(var i = 0; i < $scope.dates.length; i++){
+            $scope.formData.dates.push($scope.dates[i].date+'T'
+                    +$scope.dates[i].time+'Z'
+            );
+        }
+
+        if($routeParams.id) {
+            $http.put('/api/movies/'+$routeParams.id, $scope.formData)
+                .success(function(res) {
+                    $scope.error = false;
+                    $scope.success = true;
+                })
+                .error(function(res) {
+                    $scope.error = res;
+                    $scope.sueccess = false;
+                });
+        } else {
+            $http.post('/api/movies', $scope.formData)
+                .success(function(data) {
+                    $location.path('/movies/'+data._id);
+                })
+                .error(function(data) {
+                    $scope.error = data;
+                });
+        }
+    };
+}]);
 
 
 angular.module('doreApp').config(function($locationProvider, $routeProvider) {
@@ -26,7 +58,7 @@ angular.module('doreApp').config(function($locationProvider, $routeProvider) {
         })
         .when("/movies/edit/:id", {
             templateUrl: "new.html",
-            controller: "editMovieController"
+            controller: "addMovieController"
         })
         .when("/movies/new", {
             templateUrl: "new.html",
@@ -65,11 +97,13 @@ angular.module('doreApp').controller('movieListController', ['$scope', '$http',
 }]);
 
 
-angular.module('doreApp').controller('addMovieController', ['$scope', '$http',
-        '$location', 'ArrayManagerService',
-        function($scope, $http, $location, ArrayManagerService) {
+angular.module('doreApp')
+.controller('addMovieController', ['$scope', '$http', '$routeParams',
+        '$location', 'ArrayManagerService', 'ProcessMovieFormService',
+        function($scope, $http, $routeParams, $location,
+            ArrayManagerService, ProcessMovieFormService) {
     // empty objects to hold the form data
-    $scope.dates = {} // hold dates temporarily
+    $scope.dates = [] // hold dates temporarily
 
     $scope.formData = {};
     $scope.formData.countries = [];
@@ -81,83 +115,45 @@ angular.module('doreApp').controller('addMovieController', ['$scope', '$http',
     $scope.addElement = ArrayManagerService.add;
     $scope.removeElement = ArrayManagerService.remove;
 
-    $scope.processForm = function() {
-        $scope.formData.dates.push($scope.dates.date+'T'
-                +$scope.dates.time+'Z'
-        );
+    $scope.processForm = ProcessMovieFormService.processor;
 
-        $http.post('/api/movies', $scope.formData)
-            .success(function(data) {
-                $location.path('/movies/'+data._id);
-            })
-            .error(function(data) {
-                $scope.error = data;
-            });
-    };
-}]);
-
-
-angular.module('doreApp').controller('editMovieController', ['$scope', '$http',
-         '$routeParams', '$location', 'ArrayManagerService',
-         function($scope, $http, $routeParams, $location, ArrayManagerService) {
-    // empty object to hold the form data
-    $scope.formData = {};
-    $scope.dates = {};
-    $scope.formData.countries = [];
-    $scope.formData.dates = [];
-    $scope.formData.directors = [];
-    $scope.formData.performers = [];
-    $scope.formData.writers = [];
-
-    $scope.addElement = ArrayManagerService.add;
-    $scope.removeElement = ArrayManagerService.remove;
-
-    $http.get("/api/movies/"+$routeParams.id)
-        .success(function(res) {
-            $scope.formData = res;
-            var dateTmp = new Date($scope.formData.dates[0]);
-
-            var a = function(num) {
-                if(num < 10) return '0'+num;
-                return num;
-            }
-
-            $scope.dates.date = dateTmp.getUTCFullYear()
-                +'-'+a(dateTmp.getUTCMonth())
-                +'-'+a(dateTmp.getUTCDate());
-            $scope.dates.time = a(dateTmp.getUTCHours())
-                +':'+a(dateTmp.getUTCMinutes());
-        })
-        .error(function(res) {
-            $scope.error = res;
-        });
-
-    $scope.delete = function(id) {
-        $http.delete("/api/movies/"+id)
+    if($routeParams.id) { // edit existing movie
+        $http.get("/api/movies/"+$routeParams.id)
             .success(function(res) {
-                $scope.formData = {};
-                $location.path('/');
+                $scope.formData = res;
+
+                var num2str = function(num) {
+                    if(num < 10) return '0'+num;
+                    return num;
+                }
+
+                var dateTmp;
+                for(var i = 0; i < res.dates.length; i++) {
+                    dateTmp = new Date(res.dates[i]);
+                    $scope.dates.push({
+                        date: dateTmp.getUTCFullYear()+'-'
+                            +num2str(dateTmp.getUTCMonth())+'-'
+                            +num2str(dateTmp.getUTCDate()),
+                        time: num2str(dateTmp.getUTCHours())+':'
+                            +num2str(dateTmp.getUTCMinutes())
+                    });
+                }
             })
             .error(function(res) {
                 $scope.error = res;
             });
-    };
 
-    $scope.processForm = function() {
-        $scope.formData.dates.push($scope.dates.date+'T'
-                +$scope.dates.time+'Z'
-        );
-
-        $http.put('/api/movies/'+$routeParams.id, $scope.formData)
-            .success(function(res) {
-                $scope.error = false;
-                $scope.success = true;
-            })
-            .error(function(res) {
-                $scope.error = res;
-                $scope.sueccess = false;
-            });
-    };
+        $scope.delete = function(id) {
+            $http.delete("/api/movies/"+id)
+                .success(function(res) {
+                    $scope.formData = {};
+                    $location.path('/');
+                })
+                .error(function(res) {
+                    $scope.error = res;
+                });
+        };
+    }
 }]);
 
 
